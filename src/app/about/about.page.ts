251e21a5a6ed286c66b3b8936b6b0a2ff360c860
nurasylah94 <p93992@siswa.ukm.edu.my>
2019-10-23 18:14:21 +0800

    Third Push 231019

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NavController, NavParams} from '@ionic/angular';
+import { ActivatedRoute } from "@angular/router";
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+ item:any;
+ name:'';
+ nickname:'';
+ age:''
 
-  constructor() { }
+  constructor(private NavCtrl:NavController,private route: ActivatedRoute) {
+  
+   }
 
   ngOnInit() {
-  }
+    this.route.queryParams.subscribe(params => {
+      this.item = JSON.parse(params["profile"]);
+      this.name=this.item.name;
+      this.nickname=this.item.nickname;
+      this.age=this.item.age;
 
+
+  });
+  }
+  
+  submit(){
+
+    console.log("Button ok");
+
+this.NavCtrl.navigateBack('home');
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,57 @@
 import { Component } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { Route } from '@angular/compiler/src/core';
-
+import {NavController, AlertController} from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+profile: any={
+  name:'',
+  nickname:'',
+  age:''
+}
+  constructor(private NavCtrl:NavController, private alertController: AlertController) {}
 
-  constructor(private router: Router, private route: Route, private navigate: NavigationExtras) {}
+  //constructor(private router: Router, private route: Route, private navigate: NavigationExtras) {}
 
- 
-  
-  submit(){
-
+ async submit(){
+    
+  //to bring along the value from home
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+          profile: JSON.stringify(this.profile),
+         
+      }
+  };
     console.log("Button ok");
+    
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure to submit this data?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.NavCtrl.navigateForward(['about'], navigationExtras);
+          }
+        }
+      ]
+    });
 
-this.router.navigate(['about']);
+    await alert.present();
+    
+
+
   }
 
 }
